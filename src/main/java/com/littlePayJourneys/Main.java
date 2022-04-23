24ee8824936840249fa7d5e5018cbdd84322b841
DhanuJ <capscodes2020@gmail.com>
2022-04-23 21:55:47 +1000

    formatted Main

--- src/main/java/com/littlePayJourneys/Main.java
+++ src/main/java/com/littlePayJourneys/Main.java
@@ -9,11 +9,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Map<LocalDate, List<Taps>> listOfTapsByDate =  CSVProcessor.readTapsByDate();
+        Map<LocalDate, List<Taps>> listOfTapsByDate = CSVProcessor.readTapsByDate();
 
-        List <Trips>  processedTrips = TripsProcessor.processTripsFromTaps(listOfTapsByDate);
+        List<Trips> processedTrips = TripsProcessor.processTripsFromTaps(listOfTapsByDate);
 
         CSVProcessor.writeTripsCsv(processedTrips);
-        //System.out.println(processedTrips);
     }
 }
